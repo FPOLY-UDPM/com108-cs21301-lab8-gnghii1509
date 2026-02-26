e6b6b5535a5177c7d619d5fbc12749b1dcc8cdb0
gnghii1509 <gianghi15092007@gmail.com>
2026-02-26 23:33:04 +0700

    u ce la

--- bai1.c
+++ bai1.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Tôn Thất Gia Nghi]
+ * MSSV:      [PS48181]
+ * Lớp:       [CS21301]
  *****************************************************************************/
 
 //  BÀI 1: XÂY DỰNG CHƯƠNG TRÌNH LƯU THÔNG TIN SINH VIÊN 
@@ -11,3 +11,40 @@
 
 // VIẾT CODE Ở ĐÂY
 
+#include <stdio.h>
+#include <string.h>
+struct SinhVien {
+    char maSV[20];
+    char tenSV[50];
+    char nganhHoc[50];
+    float diemTB;
+};
+int main() {
+    int n;
+    printf("Nhap so luong sinh vien: ");
+    scanf("%d", &n);
+    struct SinhVien sv[n];
+    
+    for (int i = 0; i < n; i++) {
+        printf("Nhap thong tin sinh vien thu %d:\n", i + 1);
+        printf("Ma so sinh vien: ");
+        scanf("%s", sv[i].maSV);
+        printf("Ten sinh vien: ");
+        scanf(" %[^\n]", sv[i].tenSV); // Đọc chuỗi có dấu cách
+        printf("Nganh hoc: ");
+        scanf(" %[^\n]", sv[i].nganhHoc); // Đọc chuỗi có dấu cách
+        printf("Diem trung binh: ");
+        scanf("%f", &sv[i].diemTB);
+    }
+    
+    printf("\nThong tin sinh vien:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Ma so sinh vien: %s\n", sv[i].maSV);
+        printf("Ten sinh vien: %s\n", sv[i].tenSV);
+        printf("Nganh hoc: %s\n", sv[i].nganhHoc);
+        printf("Diem trung binh: %.2f\n", sv[i].diemTB);
+        printf("-------------------------\n");
+    }
+    
+    return 0;
+}
